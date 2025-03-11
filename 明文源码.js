d4a3d14db59b9a584954336cff0497ee7d4fd682
Zyixs <144609268+Uyzi-1@users.noreply.github.com>
2025-03-11 23:29:06 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,8 +1,8 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
-let proxyIP = '';
+let userID = '2539e7aa-7389-486f-88b4-8abfe5511d36';
+let proxyIP = 'cdn.xn--b6gac.eu.org';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
 let subConfig = atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FDTDRTU1IvQUNMNFNTUi9tYXN0ZXIvQ2xhc2gvY29uZmlnL0FDTDRTU1JfT25saW5lX01pbmlfTXVsdGlNb2RlLmluaQ==');
